071bf9003edcb57588c2294574ab5ec13003bce7
MATHEUS SILVA DA CRUZ <matheus_silva-cruz@estudante.sesisenai.org.br>
2024-09-03 10:33:32 -0300

    Arruma problemas causados pelo github

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,71 +1,16 @@
 import velhos from "../images/velhos.png"
 import '../App.css'
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 8a16d0d0513a1fdd5679a198886bdc5c65c9d79e
 function Home() {
   return (
-    <div className="paginahome">
-       <div className='pagina' id="home" style={{ backgroundImage: `url(${velhos})` }}>
-<<<<<<< HEAD
-        
-        <br/><br />
-          </div>
-        </div>
-=======
+    <div>
+      <div className='pagina' id="home" style={{ backgroundImage: `url(${velhos})` }}>
         <br /><br />
-      </div>
-      <div className="sante" id="sante">
-            <div className="sobre">
-                <h1>Quem somos nós?</h1>
-                <p>A Santé é uma ideia realizada por quatro alunos da Escola Sesi, onde tiveram a mesma ideia de criar um site simples e rápido e que pudesse ajudar  outras pessoas. O projeto Santé tem por objetivo tornar a busca por uma casa geriátrica uma tarefa mais fácil, buscando por resultados que busquem conforto conforme o cliente desejar.</p>
-                <p>Santé se refere a saúde em francês, sendo um termo utilizado em clínicas médicas, espero que consigamos atender o seu conforto.</p>
-            </div>
-            <div className="integrantes">
-                <div className="integrante">
-                    <h3>Pedro Aleksander</h3>
-                    <p>Designer</p>
-                </div>
-                <div className="integrante">
-                    <h3>Matheus Cruz</h3>
-                    <p>Líder e Desenvolvedor Back-End</p>
-                </div>
-                <div className="integrante">
-                    <h3>Vinicius de Marco</h3>
-                    <p>Desenvolvedor Back e Front-End</p>
-                </div>
-                <div className="integrante">
-                    <h3>Ian Novatzki</h3>
-                    <p>Designer</p>
-                </div>
-            </div>
+        <div id="sobre" className="sobre">
         </div>
+      </div>
     </div>
-)}
->>>>>>> 8a16d0d0513a1fdd5679a198886bdc5c65c9d79e
-
-
-
-const Header = () => {
-  const myStyle = {
-
-    color: "green",
-    padding: "40px",
-    fontFamily: "Bantayog"
-
-  };
-
-
-return(
-  <>
-  <h1 style={myStyle}>Sante</h1>
-  <p>A melhor maneira de conectar pessoas e encontrar lares</p>
-  </>
-);
+  )
 }
-
 
 export default Home;

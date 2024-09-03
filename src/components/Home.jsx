980e3d02a4a5ac8fa52be16188e945e8707f0f1b
MATHEUS SILVA DA CRUZ <matheus_silva-cruz@estudante.sesisenai.org.br>
2024-09-03 08:54:25 -0300

    Atualiza página Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,19 @@
 import velhos from "../images/velhos.png"
-import santcasa from "../images/santcasa.png"
 import '../App.css'
+
 function Home() {
   return (
     <div>
        <div className='pagina' id="home" style={{ backgroundImage: `url(${velhos})` }}>
         <br /><br />
-        <div className="container">
           <div id="sobre" className="sobre">
-            <h3>A melhor maneira de conectar pessoas e encontrar lares</h3>
-          </div>
-        </div>
+          
+        
+      </div>
       </div>
     </div>
+)}
 
-  );
-}
+
 
 export default Home;

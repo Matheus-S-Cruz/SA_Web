0f8ee1f4aeec3a1a01c9ca0fc426be916076e645
MATHEUS SILVA DA CRUZ <matheus_silva-cruz@estudante.sesisenai.org.br>
2024-09-03 09:29:20 -0300

    Atualiza página inicial

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Contato from './components/Contato';
 import Home from './components/home';
 import NavBar from './components/NavBar';
-import Sante from './components/Sante';
 import Servicos from './components/Servico';
 import Footer from './components/Footer';
 import { Routes, Route } from 'react-router-dom';
@@ -24,11 +23,7 @@ function App() {
         <>
         <NavBar />
         <Home />
-        <hr className="separador" />
-        <Sante />
-        <hr className="separador" />
         <Servicos />
-        <hr className="separador" />
         <Contato />
         <Footer />
         </>

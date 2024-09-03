d57af821422debf13bd6ef8273d655caf83623fc
MATHEUS SILVA DA CRUZ <matheus_silva-cruz@estudante.sesisenai.org.br>
2024-09-03 11:40:50 -0300

    Atualiza Contato

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -5,11 +5,10 @@ function Contato() {
     return (
         <div className="contatoedownload" id="contato">
             <div className="contato">
-                <h2>Contato e Download</h2>
-                <p>Para obter informações adicionais sobre nossos serviços e produtos, convidamos você a entrar em contato com nossa equipe de suporte especializada. Alternativamente, você pode simplificar o processo de acesso ao nosso aplicativo móvel escaneando o QR code abaixo com a câmera do seu dispositivo. Estamos aqui para auxiliá-lo(a) em todas as suas necessidades e garantir uma experiência conveniente e eficiente.</p>
-            </div>
-            <div className="download">
-                <h1>Imagem</h1>
+                <h2>Contato e Nosso Projeto</h2>
+                <div className="informacoes">
+                    <p>Para obter informações adicionais sobre nossos serviços e produtos, convidamos você a entrar em contato com nossa equipe de suporte especializada. Alternativamente, você pode simplificar o processo de acesso ao nosso aplicativo móvel escaneando o QR code abaixo com a câmera do seu dispositivo. Estamos aqui para auxiliá-lo(a) em todas as suas necessidades e garantir uma experiência conveniente e eficiente.</p>
+                </div>
             </div>
             <Link to="/home">
                 <button className="home">

847365a81cb2120412b1227d85ae5f221b7f3f80
VINICIUS DE MARCO RODRIGUES <vinicius_rodrigues11@estudante.sesisenai.org.br>
2024-09-04 11:24:19 -0300

    Atualiza Contato

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -7,7 +7,11 @@ function Contato() {
             <div className="contato">
                 <h2>Contato e Nosso Projeto</h2>
                 <div className="informacoes">
-                    <p>Para obter informações adicionais sobre nossos serviços e produtos, convidamos você a entrar em contato com nossa equipe de suporte especializada. Alternativamente, você pode simplificar o processo de acesso ao nosso aplicativo móvel escaneando o QR code abaixo com a câmera do seu dispositivo. Estamos aqui para auxiliá-lo(a) em todas as suas necessidades e garantir uma experiência conveniente e eficiente.</p>
+                    <p>Para obter informações adicionais sobre nossos serviços e produtos, convidamos 
+                    você a entrar em contato com nossa equipe de suporte especializada. Alternativamente, 
+                    você pode simplificar o processo de acesso ao nosso aplicativo móvel escaneando o 
+                    QR code abaixo com a câmera do seu dispositivo. Estamos aqui para auxiliá-lo(a) em 
+                    todas as suas necessidades e garantir uma experiência conveniente e eficiente.</p>
                 </div>
             </div>
             <Link to="/home">

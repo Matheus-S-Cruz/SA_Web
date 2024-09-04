56934e72fdaedaa1beb4c31489ea70967a65ec27
zepe16 <vinicius_rodrigues11@estudante.sesisenai.org.br>
2024-09-03 22:05:20 -0300

    Atualiza servico

--- src/components/Servico.jsx
+++ src/components/Servico.jsx
@@ -1,20 +1,36 @@
-import servicos from "../images/servicos.png"
 import oferecemos from '../images/oferecemos.png'
+import intheforest from '../images/intheforest.png'
+import piscina from '../images/piscina.png'
+import doctor from '../images/doctor.png'
 
 function Servicos() {
     return (
         <div className="servicos" id="servicos">
             <div className="oferecemos">
-                <img src={oferecemos} alt="oferecemos" />
+                <img src={oferecemos} alt="oferecemos"  />
             </div>
-            <p className="oferecemosp">Encontre o lar adotivo ideal em nossa plataforma, com diversas opções e atividades enriquecedoras incluindo:</p>
-            <div className="fotosservicos">
-                <div className="foto">
-                    <img src={servicos} alt="servicos" />
+            
+            <div className="forest" id="forest">
+                <div className="forest">
+                    <img src={intheforest} alt="forest" height={632}/> 
                 </div>
+
+                <div className="piscina" id="piscina">
+                    <div className="piscina">
+                        <img src={piscina} alt="piscina"  height={763}/>
+                    </div>
+
+                         </div>
+
+                         <div className= "doctor" id="doctor">
+                    <div className="doctor">
+                        <img src={doctor} alt="doctor" height={696}/>
+                    </div>
+
+                </div>
+
             </div>
         </div>
     );
 }
-
 export default Servicos;

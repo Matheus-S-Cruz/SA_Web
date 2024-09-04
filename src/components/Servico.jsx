c4061ce0f847365042e070a25f60d933c825fe93
Matheus_S-Cruz <matheus_silva-cruz@estudante.sesisenai.org.br>
2024-09-04 19:35:45 -0300

    Atualiza css da página principal

--- src/components/Servico.jsx
+++ src/components/Servico.jsx
@@ -1,40 +1,56 @@
-import oferecemos from '../images/oferecemos.png'
-import intheforest from '../images/intheforest.png'
-import piscina from '../images/piscina.png'
-import doctor from '../images/doctor.png'
+import oferecemos from '../images/oferecemos.png';
+import intheforest from '../images/intheforest.png';
+import piscina from '../images/piscina.png';
+import doctor from '../images/doctor.png';
+import '../App.css';
 
 function Servicos() {
     return (
         <div className="servicos" id="servicos">
-            <div className="oferecemos">
-                <img src={oferecemos} alt="oferecemos" />
+            <div className='nossosservicos'>
+                <div className="oferecemos">
+                    <img src={oferecemos} alt="oferecemos" />
+                </div>
+
+                <p className="oferecemosp">Encontre o lar adotivo ideal em nossa plataforma, com diversas opções e atividades enriquecedoras, incluindo:</p>
+
+                <div className="fotoservicos">
+                    <div className="foto" id="forest">
+                        <div className="forest-container">
+                            <img src={intheforest} alt="forest" />
+                            <div className="text-container left">
+                                <p>Trilhas guiadas por profissionais experientes e habilitados.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="foto" id="piscina">
+                        <div className="piscina-container">
+                            <img src={piscina} alt="piscina" />
+                            <div className="text-container right">
+                                <p>Sessões de fisioterapia aquática em piscinas aquecidas.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="foto" id="doctor">
+                        <div className="doctor-container">
+                            <img src={doctor} alt="doctor" />
+                            <div className="text-container doctor">
+                                <p>Consultas diagnósticas constantes para acompanhar seu bem estar.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <p className="oferecemosp">Encontre o lar adotivo ideal em nossa plataforma, com diversas opções e atividades enriquecedoras incluindo:</p>
-
-            <div className="fotoservicos" id="servicos">
-                <div className="foto" id='forest'>
-                    <div className='forest'>
-                        <img src={intheforest} alt="forest" height={632} />
-                    </div>
-                </div>
-
-                <div className="foto" id="piscina">
-                    <div className="piscina">
-                        <img src={piscina} alt="piscina" height={763} />
-                    </div>
-
-                </div>
-
-                <div className="foto" id="doctor">
-                    <div className="doctor">
-                        <img src={doctor} alt="doctor" height={696} />
-                    </div>
-
-                </div>
-
+            <div className="pservicos-container">
+                <p className='pservicos'>
+                    Nosso objetivo é garantir que você encontre um ambiente acolhedor que atenda às suas necessidades e preferências.
+                </p>
             </div>
-        </div>
+    </div>
     );
 }
+
 export default Servicos;

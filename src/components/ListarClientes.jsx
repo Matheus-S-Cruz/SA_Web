e6b345f740941d8f5e2a62e74c4b6c3ebacc14bb
Matheus_S-Cruz <matheus_silva-cruz@estudante.sesisenai.org.br>
2024-09-05 00:47:34 -0300

    Atualiza Listagem de Clientes e Cuidadores

--- src/components/ListarClientes.jsx
+++ src/components/ListarClientes.jsx
@@ -2,9 +2,55 @@ import SidebarCuidador from './SideBarCuidador';
 
 function ListarClientes() {
     return(
-        <div className='clientes'>
+        <div className='cuidadores'>
             <SidebarCuidador />
-            <h1>Lista de Clientes</h1>
+            <h1>Gerenciamento de Clientes</h1>
+            <div className="buscar-container">
+                <input type="text" placeholder="Buscar Cuidador" className="buscar-input" />
+                <button className="buscar-button">Buscar</button>
+            </div>
+            <table className="cuidadores-table">
+                <thead>
+                    <tr>
+                        <th>Idade</th>
+                        <th>Nome Completo</th>
+                        <th>Especialidade</th>
+                        <th>Contato</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>89</td>
+                        <td>Maria Silva</td>
+                        <td>AVC</td>
+                        <td>(88) 88888888</td>
+                    </tr>
+                    <tr>
+                        <td>92</td>
+                        <td>João Pereira</td>
+                        <td>Alzheimer</td>
+                        <td>(88) 88888888</td>
+                    </tr>
+                    <tr>
+                        <td>80</td>
+                        <td>Ana Costa</td>
+                        <td>Depressão</td>
+                        <td>(88) 88888888</td>
+                    </tr>
+                    <tr>
+                        <td>76</td>
+                        <td>Luciana Alves</td>
+                        <td>Demencia</td>
+                        <td>(88) 88888888</td>
+                    </tr>
+                </tbody>
+                    <tr>
+                        <td>87</td>
+                        <td>Carlos Souza</td>
+                        <td>Anormalidade auditiva</td>
+                        <td>(88) 88888888</td>
+                    </tr>
+            </table>
         </div>
     );
 }
--- src/components/ListarCuidadores.jsx
+++ src/components/ListarCuidadores.jsx
@@ -1,10 +1,57 @@
 import Sidebar from './SideBar';
+import '../App.css';
 
 function ListarCuidadores() {
     return(
         <div className='cuidadores'>
             <Sidebar />
-            <h1>Lista de Cuidadores</h1>
+            <h1>Gerenciamento de Cuidadores</h1>
+            <div className="buscar-container">
+                <input type="text" placeholder="Buscar Cuidador" className="buscar-input" />
+                <button className="buscar-button">Buscar</button>
+            </div>
+            <table className="cuidadores-table">
+                <thead>
+                    <tr>
+                        <th>Idade</th>
+                        <th>Nome Completo</th>
+                        <th>Especialidade</th>
+                        <th>Contato</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>23</td>
+                        <td>Pedro da Silva Vieira</td>
+                        <td>Cuidados Geriátricos</td>
+                        <td>(88) 8888-8888</td>
+                    </tr>
+                    <tr>
+                        <td>25</td>
+                        <td>João Oliveira</td>
+                        <td>Alzheimer</td>
+                        <td>(88) 8888-8888</td>
+                    </tr>
+                    <tr>
+                        <td>19</td>
+                        <td>Yan Pereira da Silva</td>
+                        <td>Depressão</td>
+                        <td>(88) 8888-8888</td>
+                    </tr>
+                    <tr>
+                        <td>32</td>
+                        <td>Arlinda Negrini</td>
+                        <td>Fisioterapia</td>
+                        <td>(88) 8888-8888</td>
+                    </tr>
+                    <tr>
+                        <td>27</td>
+                        <td>Pedro Alexandre Pereira Cruz</td>
+                        <td>Anormalidade Auditiva</td>
+                        <td>(88) 8888-8888</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
